e5581fa825379fdce945013cb9265746d68cf825
S Jay <jayshriya123@gmail.com>
2025-01-07 12:47:39 -0500

    Last task

--- task.py
+++ task.py
@@ -1,1 +1,19 @@
-print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.\n3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
+#print("Hello " + input("What is your name? ") + "!")
+#username = input("What is your name?")
+#length = len(username)
+#print(length)
+
+#glass1 = "milk"
+#glass2 = "juice"
+#temporary = glass1
+#glass1 = glass2
+#glass2 = temporary
+
+#print(glass1)
+#print(glass2)
+
+print("Welcome to the Band Name Generator. ")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
+
