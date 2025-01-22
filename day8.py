5b6dc7f25a04b7a219fa5b967342a2d906c21ff6
shinystar123 <78701640+shinystar123@users.noreply.github.com>
2025-01-22 12:03:02 -0500

    Update day8.py

--- day8.py
+++ day8.py
@@ -4,14 +4,18 @@ direction = input("Type 'encode' to encrypt and 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(original_text, shift_amount):
+
+def caeser(original_text, shift_amount, encode_or_decode):
     cipher_text = ""
 
     for letter in original_text:
+
+        if encode_or_decode == "decode":
+            shift_amount *= -1
+
+
         shifted_position = alphabet.index(letter) + shift_amount
         shifted_position %= len(alphabet)
         cipher_text += alphabet[shifted_position]
-    print(f"Here is the encoded result: {cipher_text}")
-
-
-encrypt(original_text=text, shift_amount=shift)
+    print(f"Here is the {encode_or_decode}d result: {cipher_text}")
+caeser(original_text=text, shift_amount=shift, encode_or_decode=direction)
