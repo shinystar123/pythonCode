acf07adf02bed0a8f822bc3499e6d59370a12dbb
shinystar123 <78701640+shinystar123@users.noreply.github.com>
2025-12-12 13:44:10 -0500

    Add Selenium script for Tinder login automation

---
+++ day 50.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+from time import sleep
+
+
+driver = webdriver.Chrome()
+
+driver.get("http://www.tinder.com")
+
+sleep(2)
+login_button = driver.find_element(By.XPATH, value='//*[text()="Log in"]')
+login_button.click()
+
+sleep(2)
+fb_login = driver.find_element(By.XPATH, value='//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+fb_login.click()
+
+sleep(2)
+base_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
+print(driver.title)
